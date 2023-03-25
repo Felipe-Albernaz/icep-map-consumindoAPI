ff40873e45ba3b20dfdb850d25abffa29e44ad9c
Felipe Albernaz <felipe.f.albernaz@gmail.com>
2023-03-25 17:05:18 -0300

    refatorando codigo e alterando estilos

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const box = document.querySelector('.info');
-const tabela = document.createElement('table');
-const box_map = document.createElement('div');
 const numero_cep = document.querySelector('#cep');
 const btn = document.querySelector('#btn');
-tabela.classList.add('tabela');
-
 
 
 btn.addEventListener('click', (evento) => {
@@ -14,7 +10,6 @@ btn.addEventListener('click', (evento) => {
     }else{
         buscaCep(numero_cep.value);
         numero_cep.value = "";
-        box.removeChild(tabela);
     }
 });
 
@@ -26,19 +21,20 @@ async function buscaCep(cep) {
         if (resposta.erro == true) {
             alert('Cep não encontrado.');
         }else{
-            tabela.innerHTML = `
+            box.innerHTML = `
                 <h2>Informações</h2>
-                <th>Cep</th><th>Logradouro</th><th>Bairro</th>
-                <th>Localidade</th><th>UF</th><th>DDD</th>
-                <tr>
-                    <td>${resposta.cep}</td><td>${resposta.logradouro}</td>
-                    <td>${resposta.bairro}</td><td>${resposta.localidade}</td>
-                    <td>${resposta.uf}</td><td>${resposta.ddd}</td>
-                </tr>
+                <table class="tabela">
+                    <tr></tr>
+                    <tr><th>Cep</th><td>${resposta.cep}</td></tr>
+                    <tr><th>Logradouro</th><td>${resposta.logradouro}</td></tr>
+                    <tr><th>Bairro</th><td>${resposta.bairro}</td></tr>
+                    <tr><th>Localidade</th><td>${resposta.localidade}</tr>
+                    <tr><th>UF</th><td>${resposta.uf}</td></tr>
+                    <tr><th>DDD</th><td>${resposta.ddd}</td></tr>
+                </table>
+
+                <div class="maprouter"><div class="gmap_canvas"><iframe class="map" width="" height="" id="gmap_canvas" src="https://maps.google.com/maps?q=${resposta.logradouro}&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://embedgooglemap.net/124/"></a><br><style>.maprouter{position:relative;text-align:right;height:;width:;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div>
                 `
-            box.appendChild(tabela);
-            box_map.innerHTML = `<div class="maprouter"><div class="gmap_canvas"><iframe class="map" width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=${resposta.logradouro}&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://embedgooglemap.net/124/"></a><br><style>.maprouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div>`;
-            box.appendChild(box_map);
         }
     } catch (error) {
         alert('Cep inválido. Verifique o número e tente novamente.');
